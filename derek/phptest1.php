1d57296288e476ffe7704ad7756ed031143cd79b
Your Name <you@example.com>
2016-09-25 09:05:14 +0300

    form1

---
+++ derek/phptest1.php
@@ -0,0 +1,15 @@
+<!---https://www.youtube.com/watch?v=smzYNZ4GEV4&list=PL21E20F9A122DC853&index=2    --->
+
+<html>
+<head>
+<title><?php echo "I have the Info";?></title>
+</head>
+<body>
+<?php
+
+?>
+
+
+
+</body>
+</html>
---
+++ derek/pt2-form.php
@@ -0,0 +1,44 @@
+<!---https://www.youtube.com/watch?v=smzYNZ4GEV4&list=PL21E20F9A122DC853&index=2   
+
+
+
+
+ --->
+ <html>
+<head>
+<title><?php echo "Send me your Info";?></title>
+</head>
+<body>
+<form action="./phptest1.php" method="post">
+
+<p>Name:
+<input type="text" name="name" size="30" value="" />
+</p>
+
+<p>Address:
+<input type="text" name="address" size="30″ value="" />
+</p>
+
+<p>City:
+<input type="text" name="city" size="25″ value="" />
+</p>
+
+<p>State:
+<input type="text" name="state" size="25″ value="" />
+</p>
+
+<p>Guess a number from 1 to 10:
+<input type="text" name="number" size="25″ value="" />
+</p>
+
+<p>Cats, Dogs, or Fish?:
+<input type="text" name="pet" size="25″ value="" />
+</p>
+
+<p>
+<input type="submit" name="submit" value="Send" />
+</p>
+
+</form>
+</body>
+</html>
