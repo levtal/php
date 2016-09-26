38084a88aac94bc02ca8aefa2c4c81ead749e888
root <root@localhost.localdomain>
2016-09-26 14:29:13 +0300

    2

--- derek/phptest2.php
+++ derek/phptest2.php
@@ -7,15 +7,52 @@
 <body>
 <?php
  echo "I know everything about you ", $_POST['name'],"<br />";
- echo "You live here " . $_POST['address']  . " "  .$_POST['city'] . ” “. $_POST['state']; 
- 
- //echo “You live here ” . $_POST[‘address’] . ” ” .$_POST[‘city’]. ” “. $_POST[‘state’] //. “<br />”;
 
- 
+ echo "You live here " . $_POST['address']." " .$_POST['city']." ". $_POST['state'] . "<br />";
+  
+ # Playing with if conditionals
+echo "Did you guess my favorite number? ";
+$numGuessed = (int) $_POST['number'];
+  
 
+# Compare multiple conditions with AND OR XOR (NOT PROVIDES THE OPPOSITE)
+if (( $numGuessed == 5 ) AND ( $numGuessed < 11))
+{
+     echo "You guessed it<br/>";
+} elseif ( $numGuessed > 5 ) {
+        echo "Too High<br />";
+     }else {
+           echo "Too Low<br />";
+       }
+
+
+
+$trueCond = true;
+$falseCond = false;
+
+$condAnswer = (int) ($trueCond XOR $falseCond);
+echo $condAnswer . "<br />";
+echo (int) !$condAnswer . "<br />";
+
+# Playing with the switch statement
+$favPet = $_POST['pet'];
+echo $favPet . " Do ";
+switch($favPet) {
+  case "Dogs" :
+    echo "Woof woof";
+  break;
+  case "Cats" :
+    echo "Meow";
+  break;
+   case "Fish" :
+    echo "Bubble";
+    break;
+default:
+    echo "Grrrr";
+}
 
 ?>
-
+ 
 
 
 </body>
