f95341da948449535aed6bf86a1ad8f58f432188
root <root@localhost.localdomain>
2016-09-28 14:33:02 +0300

    arrays1

---
+++ derek/arrays4.php
@@ -0,0 +1,58 @@
+<!--- https://www.youtube.com/watch?v=lbnTELHr_Zs&index=4&list=PL21E20F9A122DC853
+How to create an array
+How to search for information stored in an array
+How to cycle through an array with the foreach loop
+-->
+ <html>
+<head>
+<title><?php echo "Arrays";?></title>
+</head>
+
+<body>
+
+<?php
+$myInfo = array("Name" => "Derek", "Street" => "123 Main", "City" => "Pittsburgh");
+echo "My name is ", $myInfo["Name"], "<br /><br />";
+
+$moreInfo = array("State" => "PA", "Age" => 35);
+
+$myInfo = array_merge($myInfo, $moreInfo);
+
+foreach( $myInfo as $key => $value)
+{
+echo $key, " ", $value, "<br />";
+}
+echo "<br /><br />";
+
+if(array_key_exists("Name", $myInfo)) echo "The name stored is ", $myInfo["Name"];
+echo "<br /><br />";
+
+$citySearch = array_search("Pittsburgh", $myInfo);
+echo "The key for the city is ", $citySearch;
+echo "<br /><br />";
+
+print_r(array_keys($myInfo));
+echo "<br /><br />";
+
+print_r(array_values($myInfo));
+echo "<br /><br />";
+
+$customer1 = array("Name" => "Derek", "Street" => "123 Main", "City" => "Pittsburgh");
+$customer2 = array("Name" => "Sally", "Street" => "23 Main", "City" => "Pittsburgh");
+
+$customers = array($customer1, $customer2);
+print_r(array_values($customers));
+echo "<br /><br />";
+
+foreach($customers as $key)
+{
+foreach($key as $key2 => $value)
+{
+echo $key2, " ", $value, "<br />";
+}
+}
+
+?>
+
+</body>
+</html>
--- derek/looping.php
+++ derek/looping.php
@@ -6,59 +6,61 @@
     The For Loop
     How Break and Continue work
 
---->
+-->
 <html>
-<head>
-<title><?php echo "Printing Numbers";?></title>
-</head>
+	<head>
+	      <title><?php echo "Printing Numbers";?></title>
+	</head>
 
-<body>
+	<body>
 
-<?php
-$countTo = (int) $_REQUEST['countto'];
-$startNum = 1;
+	<?php
+		$countTo = (int) $_REQUEST['countto'];
+		$startNum = 1;
+   //$_REQUEST is  global variable available in all scopes of script
+  // It is an associative array that by default contains the contents of 
+  //		$_GET, $_POST and $_COOKIE. 	
+  
+  //Associative arrays = arrays that use named keys that  
+  // $age = array("Peter"=>"35", "Ben"=>"37", "Joe"=>"43");
+  //or 	 $age['Peter'] = "35"; $age['Ben'] = "37";
+  // Indexed Arrays	: $cars = array("Volvo", "BMW", "Toyota");	
+		while ( $startNum <= $countTo )	{
+				if( ($startNum % 2) == 0 ) { // only  even numbers
+					  echo $startNum, ", ";
+					}
+				elseif ($startNum >= 1000){
+					  break;
+					}
+				else{
+						$startNum++;
+						continue;
+					}
+				$startNum++;
+		}
 
-while ( $startNum <= $countTo )
-{
-if( ($startNum % 2) == 0 )
-{
-echo $startNum, ", ";
-}
-elseif ($startNum >= 1000)
-{
-break;
-}
-else
-{
-$startNum++;
-continue;
-}
-$startNum++;
-}
+		echo "<br /><br />";
+        $iterator2 = 101;
+		do {
+		    echo $iterator2. ", ";
+		    $iterator2++;
+		} while ($iterator2 <= 100);
 
-echo "<br /><br />";
+		echo "<br /><br />";
+		 
+		$maxFib = $_REQUEST['fibonacci'];
+		$firSeqNum = 0;
+		$secSeqNum = 1;
 
-$iterator2 = 101;
-do {
-echo $iterator2. ", ";
-$iterator2++;
-} while ($iterator2 <= 100);
+		for ( $iterator = 0; $iterator <= $maxFib; $iterator++)
+		{
+				echo $firSeqNum, ", ";
+				$sum = $firSeqNum + $secSeqNum;
+				$firSeqNum = $secSeqNum;
+				$secSeqNum = $sum;
+		}
 
-echo "<br /><br />";
- 
-$maxFib = $_REQUEST['fibonacci'];
-$firSeqNum = 0;
-$secSeqNum = 1;
+	?>
 
-for ( $iterator = 0; $iterator <= $maxFib; $iterator++)
-{
-echo $firSeqNum, ", ";
-$sum = $firSeqNum + $secSeqNum;
-$firSeqNum = $secSeqNum;
-$secSeqNum = $sum;
-}
-
-?>
-
-</body>
+    </body>
 </html>
