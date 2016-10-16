bdc736d8a45af2505d27a2997888c7d41c17dca8
root <root@localhost.localdomain>
2016-10-16 11:45:47 +0300

    sql1

---
+++ derek-sql/sqlcnfg.php
@@ -0,0 +1,24 @@
+<!--- 
+https://www.youtube.com/watch?v=9ywkQ9aGhVo
+http://www.newthinktank.com/2010/04/mysql-statements-in-sql/
+http://localhost/ampps/    # Configuration file
+
+
+http://localhost/phpmyadmin/ # SQL Configuration file
+
+MySQL Username:root
+
+
+Default root password = "mysql"
+
+
+mysql -u root -p
+-->
+
+ <?php  
+    $servername = "localhost";
+    $username = "root";
+    $password = "mysql";
+    $dbname = "myDB";
+ 
+?> 
