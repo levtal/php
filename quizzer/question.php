bc774fc78d20ab1db1f940ec5a3ab504fd016bee
Your Name <losangles@yahoo.com>
2016-10-23 14:57:20 +0300

    quiz 2

---
+++ quizzer/question.php
@@ -0,0 +1,46 @@
+ <!---
+https://www.youtube.com/watch?v=49vWRjNGCdE&list=PLFgUdubu2ofjuWm14mwzddzKTo5gqYvB3&index=9<!DOCTYPE html-->
+
+<html>
+<head>
+	<title>PHP Quizzer</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css" />
+</head>
+<body>
+  <header> 
+     <div class = "container">
+        <h1>PHP Quizzer </h1>
+     </div>
+  </header>
+  <main>
+  	<div class = "container">
+  	 <div class = "current">  	     Question 1 of 5
+	   <p class = "question"> 
+	       What is php stands for
+	   </p>
+	   <form method = "post" action= "process.php">
+	     <ul class = choices>
+		    <li> <input name="choise" type="radio" value="1"/>asad</li>
+		    <li> <input name="choise" type="radio" value="1"/>fdfdfd</li>
+		    <li> <input name="choise" type="radio" value="1"/>gddf</li>
+		    <li> <input name="choise" type="radio" value="1"/>dfdfdfd</li>
+		
+		 </ul>
+		 <input  type ="submit"  value="submit">
+	   </form>
+  	 </div> 
+  	</div>
+  </main>
+  <footer>
+  	 <div class = "container">
+  	   Copyright  &copy 2017 PHP Quizzer
+  	 </div>
+  </footer>
+
+</body>
+</html>
+ 
+
+
+
+ 
