22822fc9e7924f7d417fb80d4fca37fb00050a15
root <root@localhost.localdomain>
2016-10-27 16:38:28 +0300

    mysql2

---
+++ quizzer/sqlcnfg.php
@@ -0,0 +1,35 @@
+<!--- sqlcnfg.php  
+https://www.youtube.com/watch?v=9ywkQ9aGhVo
+http://www.newthinktank.com/2010/04/mysql-statements-in-sql/
+http://localhost/ampps/    # Configuration file
+
+
+http://localhost/phpmyadmin/ # SQL Configuration file
+
+MySQL Username:root
+
+
+Default root password = "mysql"
+
+
+mysql -u root -p
+-->
+
+ <?php  
+    $servername = "localhost";
+    $username = "root";
+    $password = "mysql";
+    $dbname = "myDB";
+ 
+ 
+/*  
+        // Remote host data  quizz  u643891464_quizz
+    $servername = "mysql.hostinger.co.il";
+    $username = "u643891464_moggg";
+    $password = "paseri";
+    $dbname = "u643891464_quizz";
+  
+ 
+
+*/?>
+ 
