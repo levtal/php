831e3fae5c3a3c39e16743bcf44bcfdd8424e045
Your Name <losangles@yahoo.com>
2016-11-12 20:11:55 +0200

    blog1

---
+++ blog/admin/includes/header.php
@@ -0,0 +1,31 @@
+<!--- 
+http://localhost/phpmyadmin/ # SQL Configuration file
+
+
+--> 
+<?php include 'database.php'?> 
+<?php   ?>
+<html>
+<head>
+	<title>fffff</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css" />
+</head>
+<body>
+  <header> 
+     <div  >
+        <h1>P er </h1>
+     </div>
+  </header>
+  <main>
+     
+  </main>
+  <footer>
+  	 
+  </footer>
+
+</body>
+</html>
+ 
+
+
+
---
+++ blog/post.php
@@ -0,0 +1,33 @@
+<!---Add question
+https://www.youtube.com/watch?v=aejWA9vYEJQ&list=PLFgUdubu2ofjuWm14mwzddzKTo5gqYvB3&index=11
+15:01
+http://localhost/phpmyadmin/ # SQL Configuration file
+
+
+--> 
+<?php include 'database.php'?> 
+<?php   ?>
+<html>
+<head>
+	<title>fffff</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css" />
+</head>
+<body>
+  <header> 
+     <div  >
+        <h1>P er </h1>
+     </div>
+  </header>
+  <main>
+     
+  </main>
+  <footer>
+  	 
+  </footer>
+
+</body>
+</html>
+ 
+
+
+
