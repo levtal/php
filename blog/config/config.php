b59df6e12333e20c6387a597c16904feb92cec48
root <root@localhost.localdomain>
2016-11-17 16:22:46 +0200

    config change

--- blog/config/config.php
+++ blog/config/config.php
@@ -1,9 +1,9 @@
  <!--- config.php -->
  
  <?php   
-    define('DB_HOST', 'localhost');
+    define('DB_HOST', '127.0.0.1');
     define('DB_USER', 'root');
-    define('DB_PASS', '');  ## unbunto::"mysqlmysql" , vista::"", kali  
+    define('DB_PASS', '');  ## unbunto::"mysqlmysql" , vista::"", kali ::"" 
     define('DB_NAME', 'blog');
  
   
