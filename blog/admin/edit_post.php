ba3471588f27399ddf75763999d80500ac08d6e3
yurgename <yourgine@bing.com>
2016-11-22 16:12:49 +0200

    dfsdef

--- blog/admin/edit_post.php
+++ blog/admin/edit_post.php
@@ -1,22 +1,41 @@
   <!--admin/edit_post.php -->
 <?php include 'includes/header.php'; ?>
+
+<?php  
+ $db = new Database();  //libreries/Database.php
+ $id = $_GET['id'];
+ $query = "SELECT * FROM posts
+          WHERE id = " . $id;  //Get  all posts
+ $post = $db->select($query)->fetch_assoc();
+  
+ $query = "SELECT * FROM categories";  //Get  all categories
+ $categories = $db->select($query);
+?>
+<?php ?>
+
  <form role="form" method="post" action="edit_post.php">
   <div class="form-group">
     <label>Post Title</label>
-    <input name ="title" type="text" class="form-control"   placeholder="Enter Title">
+    <input name ="title" type="text" class="form-control"   
+     placeholder="Enter Title"  
+     value="<?php echo $post['title'];?>">
   </div>
   
   <div class="form-group">
     <label>Post Body</label>
-    <textarea name="body" class="form-control" placeholder="Enter Post Body">
-	</textarea>
+    <textarea name="body" class="form-control" placeholder="Enter Post Body" >
+       <?php echo $post['body'];?>"
+	 </textarea>
   </div>
   
+    <!--       ----Category----       -->
   <div class="form-group">
     <label>Category</label>
 	<select name="category" class="form-control">
-		<option>News</option>
-		<option>Events</option>
+	 <?php while($row = $categories->fetch_assoc()):?>
+       <option> <?php echo $row['name'];?></option>
+    <?php endwhile;?>
+		 
 	</select>
   </div>
   
--- blog/admin/index.php
+++ blog/admin/index.php
@@ -1,35 +1,56 @@
 <!--admin/index.php -->
 <?php include 'includes/header.php'; ?>
-  <table class="table table-striped">
-    <tr>
-	  <th>Post ID</th>
-	  <th>Post Title</th>
-	  <th>Category</th>
-	  <th>Author</th>
-	  <th>Date</th>
-	</tr> 
-	<tr>
-	  <td>Post ID</td>
-	  <td>Post Title</td>
-	  <td>Category</td>
-	  <td>Author</td>
-	  <td>Date</td>
-	</tr> 
-  </table>
+<?php
+ $db = new Database();  //libreries/Database.php
+ 
+ $query = "SELECT  posts.* , categories.name FROM posts
+           INNER JOIN categories
+           ON posts.Category=categories.id";  //Get  all posts
+ $posts = $db->select($query);
+
+ $query = "SELECT * FROM categories";  //Get  all categories
+ $categories = $db->select($query);
+?>
+
+ <table class="table table-striped">
+   <tr>
+	<th>Post ID</th>
+	<th>Post Title</th>
+	<th>Category</th>
+	<th>Author</th>
+	<th>Date</th>
+   </tr> 
    
+   <?php  while($row = $posts->fetch_assoc()):?>
+   	<tr>
+		<td><?php  echo $row['id'];?></td>
+		<td><a href="edit_post.php?id=<?php echo $row['id'];?>">
+		      <?php echo $row['title'];?>
+		    </a> 	
+		</td>
+		<td><?php  echo $row['name'];?></td>
+		<td><?php  echo $row['author'];?></td>
+		<td><?php  echo formatDate($row['date']);?></td>
+	</tr> 
+	<?php  endwhile;?>
+  </table>
+  
+ <br>  
 <table class="table table-striped">
     <tr>
 	  <th>Category ID#</th>
 	  <th>Category name</th>
 	   
 	</tr> 
-	<tr>
-	  <td>Post ID</td>
-	  <td>Post Title</td>
-	  
+	<?php  while($row = $categories->fetch_assoc()):?>
+   	<tr>
+		<td><?php  echo $row['id'];?></td>
+		<td><a href="category.php?id=<?php echo $row['id'];?>">
+		      <?php echo $row['name'];?>
+		    </a> 	
+		</td>
 	</tr> 
+	<?php  endwhile;?>
   </table> 
-
- 
 
 <?php include 'includes/footer.php'; ?> 
