d9db33a817642aa04aab06682ae2c2405fd71998
Your Name <losangles@yahoo.com>
2016-12-01 21:48:09 +0200

    start taling

---
+++ talkingspace/config/config.php
@@ -0,0 +1,22 @@
+ <!--- config.php -->
+ 
+ <?php   
+    define('DB_HOST', '127.0.0.1');
+    define('DB_USER', 'root');
+    define('DB_PASS', '');  ## unbunto::"mysqlmysql" , vista::"", kali ::"" 
+    define('DB_NAME', 'blog');
+ 
+  
+ /*
+        // Remote host data  quizz  u710518084_quizz
+     define('DB_HOST' , 'mysql.hostinger.co.il');
+     define('DB_USER' , 'u710518084_moggg');
+     define('DB_PASS' , 'paseri');
+     define('DB_NAM'E ,'u710518084_quizz');
+  
+ 
+   */
+   $site_description = " Arthur Deikman describes the phenomenon as an  intuitive knowing, a type of perception that bypasses the usual sensory channels and rational intellect ";
+   
+?>
+ 
---
+++ talkingspace/libraries/Database.php
@@ -0,0 +1,89 @@
+<?php
+ 
+ 
+class Database{
+ public $host = DB_HOST;
+ public $username = DB_USER;
+ public $passward = DB_PASS;
+ public $db_name = DB_NAME;
+	
+ public $link;  //mysqli object
+ public $error;
+	
+  /*Class constructor*/
+ public function __construct(){
+	$this->connect();  // Call connect function
+  }
+	
+     /*Connector*/	
+ private function connect(){
+  $this->link = new mysqli($this->host, $this->username, $this->passward, $this->db_name);
+  if (! $this->link){
+	$this->error = "Connection failed: ".  $this->link->connect_error;
+    return false;
+   }
+ }// connect 
+ 
+ 
+ /*select*/
+ public function select($query){
+	 
+  $result = $this->link->query($query)  
+	       or die($this->link->error.__LINE__);
+ 
+  if ($result->num_rows > 0){
+	return $result;  
+  }else{
+	 return false; 
+  }
+ }//select
+  
+ /*insert*/
+ public function insert($query){
+  $insert_row = $this->link->query($query)  
+	       or die($this->link->error.__LINE__);
+ 
+  if ($insert_row){
+	header("Location: index.php?msg=".urlencode( 'Record added'));  
+	exit();
+  }else{
+	  die('Error: ('. $this->link->errno . ') '. $this->link->errnor); 
+  }
+ }//insert 
+  
+  
+ /*update*/
+public function update($query){
+  $update_row = $this->link->query($query)  
+	       or die($this->link->error.__LINE__);
+ 
+  if ($update_row){
+	header("Location: index.php?msg=".urlencode( 'Record updated'));  
+    exit();
+ }else{
+	  die('Error: ('. $this->link->errno . ') '. $this->link->errnor); 
+  }
+ }//update 
+
+
+ 
+public function delete($query){
+  $delete_row = $this->link->query($query)  
+	       or die($this->link->error.__LINE__);
+ 
+  if ($delete_row){
+	header("Location: index.php?msg=".urlencode( 'Record deleted'));  
+    exit();
+ }else{
+	  die('Error: ('. $this->link->errno . ') '. $this->link->errnor); 
+  }
+ }//update  
+ 
+ 
+  
+}//class Database
+
+?>
+
+
+         
