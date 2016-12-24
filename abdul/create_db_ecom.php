9f87309e59969fe9917ee4b1867944b8e9026913
Your Name <losangles@yahoo.com>
2016-12-24 20:57:41 +0200

    abdul

---
+++ abdul/create_db_ecom.php
@@ -0,0 +1,227 @@
+
+<?php
+ /* script  to create   tables in the  'ecom'  database
+ 
+ 1'sd step:   create dbase 'ecom'  in localhost/phpmyadmin/
+ 2'nd step:   run this script
+------------------------------------------
+ 
+ This script will create the next tables:
+ 1. products: 
+ 2. categories:  
+ 3. brands
+  
+	 
+
+	 
+	 */
+ define('DB_HOST', '127.0.0.1');
+ define('DB_USER', 'root');
+ define('DB_PASS', '');## unbunto:"mysqlmysql",vista:"", kali :"" 
+ define('DB_NAME', 'ecom');
+
+  /*
+        // Remote host data  quizz  u710518084_quizz
+     define('DB_HOST' , 'mysql.hostinger.co.il');
+     define('DB_USER' , 'u643891464_moggg'); 
+     define('DB_PASS' , 'paseri');
+     define('DB_NAME' ,'u643891464_quizz'); 
+  
+  */
+
+ echo "<b>Creating 8 tables in ecom db</b>  <br>--------------------<br>  ";
+// Create connection
+$conn = new mysqli(DB_HOST, DB_USER , DB_PASS , DB_NAME);
+ 
+
+if ($conn->connect_error) { // Check connection
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// sql to create table products (8 fields)
+$sql = "CREATE TABLE products (
+	product_id INT(100) UNSIGNED   AUTO_INCREMENT PRIMARY KEY,
+	product_cat INT(100),
+	product_brand INT(100),
+	product_title VARCHAR(255),
+	product_price   DECIMAL(10.2),
+	product_desc  TEXT,
+	product_image TEXT(100),
+	product_keywords TEXT(200)
+	
+	)";
+	 
+echo "<br>".$sql . "<br><br>";
+if ($conn->query($sql) === TRUE) {
+    echo "Table products created successfully<br>";
+} else {
+    echo "Error creating table [products] : " . $conn->error,"<br>";
+}
+
+echo "<br>";
+
+ // sql to create table  categories  (2 fields)
+$sql = "CREATE TABLE categories (
+    cat_id INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    cat_title TEXT
+ )";
+echo "<br>".$sql . "<br>";
+echo "<br>";
+
+if ($conn->query($sql) === TRUE) {
+    echo "Table categories created successfully<br>";
+} else {
+    echo "Error creating table categories: " . $conn->error."<br>";
+	
+}
+
+echo "<br>";
+
+ // sql to create table  brands  (2 fields)
+$sql = "CREATE TABLE brands (
+    brand_id INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    brand_title TEXT
+ )";
+echo "<br>".$sql . "<br>";
+echo "<br>";
+
+if ($conn->query($sql) === TRUE) {
+    echo "Table brands created successfully<br>";
+} else {
+    echo "Error creating table brands: " . $conn->error."<br>";
+	
+}
+
+/*
+// sql to create table orders (11 fields)
+$sql = "CREATE TABLE orders (
+	id INT(11) UNSIGNED   AUTO_INCREMENT PRIMARY KEY,
+	product_id INT(11),
+    user_id INT(11),
+	transaction_id VARCHAR(255),
+	qty INT(11),
+	price   DECIMAL(10.2),
+	address VARCHAR(255),
+	address2 VARCHAR(255),
+	city VARCHAR(100),
+	state VARCHAR(100),
+	zipcode  VARCHAR(30)
+	)";
+	 
+echo"<br>". $sql . "<br>";
+echo "<br>";
+if ($conn->query($sql) === TRUE) {
+    echo "Table [orders] created successfully";
+} else {
+    echo "Error creating table [orders] : " . $conn->error;
+}
+
+
+
+// sql to create table users (11 fields)
+$sql = "CREATE TABLE users (
+	id INT(11) UNSIGNED   AUTO_INCREMENT PRIMARY KEY,
+	first_name VARCHAR(100),
+    last_name VARCHAR(100),
+	email VARCHAR(100),
+	username VARCHAR(100),
+	password  VARCHAR(100),
+	join_date TIMESTAMP   default CURRENT_TIMESTAMP
+	)";
+	 
+
+echo "<br>". $sql . "<br>";
+echo "<br>";
+
+if ($conn->query($sql) === TRUE) {
+    echo "Table [users] created successfully";
+} else {
+    echo "Error creating table [users] : " . $conn->error;
+}
+ 
+echo "<br>";
+
+////////////////////      Insert to DB ecom
+//https://en.wikipedia.org/wiki/List_of_art_movements
+
+*/
+//  insert data to  table 'categories'
+$sql = "INSERT INTO categories(cat_title)
+        VALUES 
+		       ('Cubism'),
+			   ('Dada'),
+			   ('De Stijl'),
+			   
+			   ('Expressionism')";
+
+echo "<br>".$sql . "<br>";
+echo "<br>";
+			   
+if ($conn->query($sql) === TRUE) {
+    echo  "<br> Categories inserted into question table successfully<br>";
+} else {
+    echo "<br> Error in insert into categories table: " . $conn->error. "<br>";
+}
+echo "<br>";
+
+
+//  insert data to  table 'brand'
+$sql = "INSERT INTO brands(brand_title)
+        VALUES 
+		       ('Georges Braque'),
+			   ('Pablo Picasso'),
+			   ('Andre Lhote'),
+			   ('Egon Schiele'),
+			   ('Oskar Kokoschka'),
+			   ('Josef Gassler')";
+
+echo "<br>".$sql . "<br>";
+echo "<br>";
+			   
+if ($conn->query($sql) === TRUE) {
+    echo  "<br> brands inserted into question table successfully<br>";
+} else {
+    echo "<br> Error in insert into brands table: " . $conn->error. "<br>";
+}
+echo "<br>";
+
+
+/*
+//insert user
+$sql = "INSERT INTO users 
+        (first_name,last_name,email,username,password) 
+        VALUES ( 'ro' ,'R','a@ur','mm','1234' ),
+		       ( 'romeo' ,'fbml','fbmm@ur','nn','1234' )
+				 ";
+echo "<br>"	;	
+echo "<br>". $sql . "<br>";
+if ($conn->query($sql) === TRUE) {
+    echo  "<br> Users inserted into users table successfully";
+} else {
+    echo "<br> Error in insert into users table: " . $conn->error;
+}
+
+//  products
+
+$sql = "INSERT INTO products 
+               ( category,title,description,image,price )
+        VALUES ( '1' ,'destiny','aur','game1.jpg','59.99' ),
+		       ( '1' ,'dcl','aur','game4.jpg','49.99' ),
+			   ( '5' ,'use','norway ','game3.jpg','159.99' ),
+			   ( '2' ,'fvrv','fddvf','game8.jpg','59.99' ),
+			   ( '3' ,'shadow','fbtrgh  ghghg ','game3.jpg','159.99' ),
+		       ( '1' ,'dcl','ser  tyffd  bbh','game4.jpg','49.99' )
+				 ";
+echo "<br>"	;	
+echo "<br>". $sql. "<br>";
+if ($conn->query($sql) === TRUE) {
+    echo  "<br> Products inserted into products table successfully";
+} else {
+    echo "<br> Error in insert into products table: " . $conn->error;
+}
+
+*/
+$conn->close();
+?> 
+
+
---
+++ abdul/ecom/index.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+ 
+<html lang="en">
+<head>
+         <title>E-Commerce</title>
+
+    <!-- Bootstrap core CSS -->
+    <link rel="stylesheet" href="styles/style.css" media="all"/>
+
+      <!-- Custom styles for this template -->
+    <link href="css/custom.css" rel="stylesheet">
+	 
+    <script src="/js/bootstrap.js"></script>
+    
+</head>
+
+<body>
+ <div class="main_wrapper">
+    <div class="header_wrapper"> <!--  header -->
+       <img id="logo" src="images/logo.png">
+	   <img id="banner" src="images/ad_banner.png">
+	   
+    </div>
+	
+	<div class="menubar"> <!-- upper menu bar   -->
+        <ul id="menu">
+    	  <li><a href="#">Home</a></li>
+          <li><a href="#">All Products</a></li>
+          <li><a href="#">My Account</a></li>
+          <li><a href="#">Sign up</a></li>
+          <li><a href="#">Shopping Cart</a></li>
+          <li><a href="#">Contect Us</a></li>
+       
+	   </ul>
+	   <div id="form">
+	    <form class="" method="get" action="result.php" enctype="multipart/form-data">
+          <input type="text" name="user_query"   placeholder="Search"/>
+          <input type="submit" name="search"  value="search"/>          
+		</form>
+	   </div>
+	   
+	   
+	   
+	   
+	</div>
+	
+    <div class="content_wrapper"> 
+	 
+	 <div id="sidebar"><!-- side bar   -->
+       <div id="sidebar_title">Categories</div>
+		<ul id="cats">
+          <li class=" "><a href="#" >Dapibus ac faci</a></li>
+          <li class=" "><a href="#" >fdfddvdvisis in</a></li>
+          <li class=" "><a href="#" >fgv  tgcvc  dfffg</a></li>
+          <li class=" "><a href="#" >ge</a></li>   
+        </ul> 
+		  
+      
+       <div id="sidebar_title">Brands</div>
+		<ul id="cats">
+          <li class=" "><a href="#" >HP</a></li>
+          <li class=" "><a href="#" >Dell</a></li>
+          <li class=" "><a href="#" >fgv  tgcvc  dfffg</a></li>
+          <li class=" "><a href="#" >ge</a></li>   
+        </ul> 
+		  
+     </div>
+	
+	<div id="content_area"><!--  content  -->
+          this is content area
+     </div>
+	</div><!--end content_wrapper   -->
+	
+	<div id="footer"><!--   footer -->
+      <p> The scab &copy 2017, All Rights Reserved</p>
+    </div>
+ </div> <!-- main_wrapper end  -->
+ 
+</body>
+</html>
+ 
