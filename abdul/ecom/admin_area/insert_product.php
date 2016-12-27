931256508cbe3e1ade6c6f16ff9c14b6686764e3
Your Name <losangles@yahoo.com>
2016-12-27 11:53:32 +0200

    img

--- abdul/ecom/admin_area/insert_product.php
+++ abdul/ecom/admin_area/insert_product.php
@@ -6,7 +6,7 @@
   
 <html lang="en">
 <head>
-         <title>Insert</title>
+     <title>Insert</title>
 
     <!-- Bootstrap core CSS -->
     <link rel="stylesheet" href="styles/style.css" media="all"/>
@@ -15,15 +15,18 @@
     <link href="css/custom.css" rel="stylesheet">
 	 
     <script src="/js/bootstrap.js"></script>
-    
+    <script src="//tinymce.cachefly.net/4.1/tinymce.min.js"></script>
+    <script>
+        tinymce.init({selector:'textarea'});
+</script>
 </head>
 
 <body style="background:BurlyWood;"	>
   <div id="form">
-	<form action="insert_product.php" method="post" enctype="multipart/form-data"> 
-	  <table align="center" width="795" border="2" bgcolor="#997969"">
+   <form action="insert_product.php" method="post" enctype="multipart/form-data"> 
+	<table align="center" width="795" border="1" bgcolor="#997969">
 		<tr align="center">
-			<td colspan="7"><h2>Insert New Post Here</h2></td>
+			<td colspan="7"><h2> New Product</h2></td>
 		</tr>
 		<tr>
 			<td align="right"><b>Product Title:</b></td>
@@ -34,14 +37,14 @@
 			<td>
 				<select name="product_cat" >
 					<option>Select a Category</option>
-				     <?php  getCats(); ?>
+				    <?php  getCats(); ?>
 				</select>
         	</td>
 		</tr>
 			
 		<tr>
 			<td align="right"><b>Product Brand:</b></td>
-			 <td>
+			<td>
 				<select name="product_brand" >
 					<option>Select a Brand</option>
 					<?php  getBrands(); ?>
@@ -49,35 +52,77 @@
 		    </td>
 		</tr>
 			
-			<tr>
-				<td align="right"><b>Product Image:</b></td>
-				<td><input type="file" name="product_image" /></td>
-			</tr>
+		<tr>
+			<td align="right"><b>Product Image:</b></td>
+			<td>
+			    <input type="file" name="product_image" />
+				 Image URL  
+				<input type="text" name="product_image2" size="40" />
+			</td>
+		</tr>
 			
-			<tr>
-				<td align="right"><b>Product Price:</b></td>
-				<td><input type="text" name="product_price" required/></td>
-			</tr>
+		<tr>
+			<td align="right"><b> Price:</b></td>
+			<td><input type="text" name="product_price" required/></td>
+		</tr>
 			
-			<tr>
-				<td align="right"><b>Product Description:</b></td>
-				<td><textarea name="product_desc" cols="20" rows="10"></textarea></td>
-			</tr>
+		<tr>
+			<td align="right"><b> Description:</b></td>
+			<td>
+			 <textarea name="product_desc" cols="20" rows="10">
+			 </textarea>
+		   </td>
+		</tr>
 			
-			<tr>
-				<td align="right"><b>Product Keywords:</b></td>
-				<td><input type="text" name="product_keywords" size="50" required/></td>
-			</tr>
+		<tr>
+			<td align="right"><b> Keywords:</b></td>
+			<td><input type="text" name="product_keywords" size="50" required/></td>
+		</tr>
 			
-			<tr align="center">
-				<td colspan="7"><input type="submit" name="insert_post" value="Insert Product Now"/></td>
-			</tr>
-		
-	 </table>
-	
-	
-</form>
- 
-</body>
+		<tr align="center">
+			<td colspan="7"><input type="submit" name="insert_post" value="Insert Product Now"/></td>
+		</tr>
+	</table>
+   </form>
+ </body>
 </html>
+
+<?php 
+
+	if(isset($_POST['insert_post'])){
+	
+		//getting the text data from the fields
+		$product_title = $_POST['product_title'];
+		$product_cat= $_POST['product_cat'];
+		$product_brand = $_POST['product_brand'];
+		$product_price = $_POST['product_price'];
+		$product_desc = $_POST['product_desc'];
+		$product_keywords = $_POST['product_keywords'];
+	
+		//getting the image from the field
+		$product_image = $_FILES['product_image']['name'];
+		$product_image_tmp = $_FILES['product_image']['tmp_name'];
+		$product_image2 =  $_POST['product_image2'];
+		
+		echo  "product_image= ". $product_image;
+		echo  "product_image_tmp= ".$product_image_tmp;
+		echo  "product_image2= ".$product_image2;
+		
+		
+		
+		/*
+		move_uploaded_file($product_image_tmp,"product_images/$product_image");
+	
+		 $insert_product = "insert into products (product_cat,product_brand,product_title,product_price,product_desc,product_image,product_keywords) values ('$product_cat','$product_brand','$product_title','$product_price','$product_desc','$product_image','$product_keywords')";
+		 
+		 $insert_pro = mysqli_query($con, $insert_product);
+		 
+		 if($insert_pro){
+		 
+		   echo "<script>alert('Product Has been inserted!')</script>";
+		   echo "<script>window.open('index.php?insert_product','_self')</script>";
+		 
+		 }*/
+	}
+
  
