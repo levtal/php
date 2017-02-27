d3ea1305614e0aac0f75bda6c1f9df999b19ead5
Richm <ric5hme45@yahoo.com>
2017-02-27 19:52:37 +0200

    dwn

--- spider/dwn.php
+++ spider/dwn.php
@@ -11,10 +11,12 @@ $ref    = "http://www.schrenk.com";
 $return_array = http_get_withheader($target, $ref); 
 # Display the header
 echo "FILE CONTENTS \n";
-var_dump($return_array['FILE']); 
+//var_dump($return_array['FILE']); 
  
 echo "ERRORS \n";
 var_dump($return_array['ERROR']); 
 echo "STATUS \n";
-var_dump($return_array['STATUS']);
+//var_dump($return_array['STATUS']);
+
+echo "<br><pre>".print_r($return_array['STATUS'], true) . "</pre>";
 ?>
