7b86d533dac6852e3a648805070a15b4281428bc
Your Name <losangles@yahoo.com>
2017-06-05 14:31:21 +0300

    ajax2

--- ajax/1 - 11/foodstore.js
+++ ajax/1 - 11/foodstore.js
@@ -1,17 +1,18 @@
-var xmlHttp = createXmlHttpRequestObject();
+var xmlHttp = createXmlHttpRequestObject(); // This is the object that communicate 
+                                            //server php and java script]
 
 function createXmlHttpRequestObject(){
 	var xmlHttp;
 
-	if(window.ActiveXObject){
+	if(window.ActiveXObject){//If this is Internet Explorer browser
 		try{
 			xmlHttp = new ActiveXObject("Microsoft.XMLHTTP");
 		}catch(e){
 			xmlHttp = false;
 		}
-	}else{
+	}else{ //This is NOT Internet Explorer
 		try{
-			xmlHttp = new XMLHttpRequest();
+			xmlHttp = new XMLHttpRequest();// This is a buildin  function
 		}catch(e){
 			xmlHttp = false;
 		}
@@ -24,22 +25,23 @@ function createXmlHttpRequestObject(){
 	}
 }
 
-function process(){
-	if(xmlHttp.readyState==0 || xmlHttp.readyState==4){
-		food = encodeURIComponent(document.getElementById("userInput").value);
-		xmlHttp.open("GET", "foodstore.php?food=" + food, true);
-		xmlHttp.onreadystatechange = handleServerResponse;
-		xmlHttp.send(null);
-	}else{
+function process(){ // this function is called from index..html
+      // Send a request to the server
+ if(xmlHttp.readyState==0 || xmlHttp.readyState==4){
+  food = encodeURIComponent(document.getElementById("userInput").value); //get value of input
+  xmlHttp.open("GET", "foodstore.php?food=" + food, true);//Send dtat to the server 
+  xmlHttp.onreadystatechange = handleServerResponse;
+  xmlHttp.send(null);
+ }else{ //The serveer  is busy
 		setTimeout('process()', 1000);
 	}
 }
 
 function handleServerResponse(){
-	if(xmlHttp.readyState==4){
-		if(xmlHttp.status==200){
+	if(xmlHttp.readyState==4){ //Communication is over
+		if(xmlHttp.status==200){ //Communication is ok
 			xmlResponse = xmlHttp.responseXML;
-			xmlDocumentElement = xmlResponse.documentElement;
+			xmlDocumentElement = xmlResponse.documentElement;//Root element
 			message = xmlDocumentElement.firstChild.data;
 			document.getElementById("underInput").innerHTML = '<span style="color:blue">' + message + '</span>';
 			setTimeout('process()', 1000);
--- ajax/1 - 11/foodstore.php
+++ ajax/1 - 11/foodstore.php
@@ -1,9 +1,10 @@
 <?php 
-header('Content-Type: text/xml');
+header('Content-Type: text/xml');//Generate XML Content
 echo '<?xml version="1.0" encoding="UTF-8" standalone="yes" ?>';
 
 echo '<response>';
-	$food = $_GET['food'];
+
+	$food = $_GET['food'];//xml tag
 	$foodArray = array('tuna','bacon','beef','loaf','ham');
 	if(in_array($food, $foodArray))
 		echo 'We do have '.$food.'!';
@@ -11,5 +12,6 @@ echo '<response>';
 		echo 'Enter a food you fool.';
 	else
 		echo 'Sorry we dont sell no '.$food.'!';
-echo '</response>';
+
+echo '</response>'; // End xml tag
 ?>
